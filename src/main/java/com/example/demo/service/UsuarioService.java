618dcd3d9340519ef66fd7359d239a4714ab8352
SantiagoLeza <santiago.leza@hotmail.com>
2023-05-11 11:20:14 -0300

    1.0

---
+++ src/main/java/com/example/demo/service/UsuarioService.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.service;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.dao.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author santi
+ */
+@Service
+public class UsuarioService implements IUsuarioService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        List<Usuario> users =  usuarioRepository.findAll();
+        return users;
+    }
+
+    @Override
+    public void saveUsuario(Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void deleteUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    @Override
+    public Usuario findUsuario(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+}
